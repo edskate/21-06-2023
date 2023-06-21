12ccfe3a82d04f438b396060bd95c9503c95d945
edilson gomes de lima <edskateicevida@gmail.com>
2023-06-21 03:22:20 -0300

    modificação

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# 21-06-2023
- 
+mindmap
+root((mindmap))
+Origins
+Long history
+::icon(fa fa-book)
+Popularisation
+British popular psychology author Tony Buzan
+Research
+On effectiveness<br/>and features
+On Automatic creation
+Uses
+Creative techniques
+Strategic planning
+Argument mapping
+Tools
+Pen and paper
+Mermaid
---
+++ text.py
@@ -0,0 +1,48 @@
+print("Hello, world")
+
+x = 'Python'
+y = 'is'
+z = "awesome"
+print(x, y , z)
+
+x = abs(-7.25)
+print(x)
+
+if 5 > 2:
+    print("Five is greater than two!")
+    
+    #print('hello world)
+    print("Hello, world")
+    
+    x = 5
+    z = "john"
+    print(x)
+    print(y)
+    
+    x = 4 #x is of type int
+    y = "sally" # x is now of type str
+    print(x)
+    
+    x = str(3) # x will be '3'
+    y = int(3) # y will be 3
+    z = float(3) # z will be 3.0
+    
+    x = y = z = 'Orange'
+    print(x)
+    print(y)
+    print(z) 
+    
+    
+    fruits = ["apple", "banana", "cherry"]
+    x, y, z = fruits
+    print(x)
+    print(y)
+    print(z)
+    
+    x = "Python is awesome"
+    print(x)
+    
+    x = "awesome"
+    def myfunc():
+     print("python is " + x)
+    myfunc()
